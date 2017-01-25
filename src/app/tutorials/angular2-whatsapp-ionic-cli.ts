ecad465d14501c0bdeb41eb3396d3f0fa2aac107
dotansimha <dotansimha@gmail.com>
2017-01-26 00:08:06 +0200

    Add new whatsapp tutorial

--- src/app/tutorials/angular2-whatsapp-ionic-cli.ts
+++ src/app/tutorials/angular2-whatsapp-ionic-cli.ts
@@ -14,50 +14,73 @@ export const ANGULAR2_WHATSAPP_IONIC_CLI: TutorialDefinition = {
   steps: [
     {
       url: '/setup',
-      name: 'Initial setup',
-      template: markdownUrlBase + 'templates/step1.md',
-      //youtubeVideoId: 'rpTAGoSmVcE'
+      name: 'Bootstrapping',
+      template: markdownUrlBase + 'templates/step1.md'
     },
     {
       url: '/chats-page',
       name: 'Chats page',
-      template: markdownUrlBase + 'templates/step2.md',
-      //youtubeVideoId: 'hZLyASyZiqo'
+      template: markdownUrlBase + 'templates/step2.md'
+    },
+    {
+      url: '/rxjs',
+      name: 'RxJS',
+      template: markdownUrlBase + 'templates/step3.md'
     },
     {
       url: '/meteor-server-side',
       name: 'Realtime Meteor Server',
-      template: markdownUrlBase + 'templates/step3.md',
-      //youtubeVideoId: 'T_118SkJZ4o'
+      template: markdownUrlBase + 'templates/step4.md'
+    },
+    {
+      url: '/folder-structure',
+      name: 'Folder Structure',
+      template: markdownUrlBase + 'templates/step5.md'
     },
     {
       url: '/messages-page',
       name: 'Messages Page',
-      template: markdownUrlBase + 'templates/step4.md',
-      //youtubeVideoId: 'FqyDObNCzqE'
+      template: markdownUrlBase + 'templates/step6.md'
     },
     {
       url: '/authentication',
       name: 'Authentication',
-      template: markdownUrlBase + 'templates/step5.md',
-      //youtubeVideoId: 'ONY1L9cD5BY'
+      template: markdownUrlBase + 'templates/step7.md'
     },
     {
       url: '/chats-mutations',
       name: 'Chats Creation & Removal',
-      template: markdownUrlBase + 'templates/step6.md',
-      //youtubeVideoId: 'Y_6SU1dEWCA'
+      template: markdownUrlBase + 'templates/step8.md'
     },
     {
       url: '/privacy',
       name: 'Privacy & Security',
-      template: markdownUrlBase + 'templates/step7.md',
-      //youtubeVideoId: 'CGeClgrUSeQ'
+      template: markdownUrlBase + 'templates/step9.md'
+    },
+    {
+      url: '/filter-and-pagination',
+      name: 'Filter & Pagination',
+      template: markdownUrlBase + 'templates/step10.md'
+    },
+    {
+      url: '/google-maps',
+      name: 'Google Maps',
+      template: markdownUrlBase + 'templates/step11.md'
+    },
+    {
+      url: '/file-upload',
+      name: 'File Upload & Images',
+      template: markdownUrlBase + 'templates/step12.md'
+    },
+    {
+      url: '/native-mobile',
+      name: 'Native Mobile',
+      template: markdownUrlBase + 'templates/step13.md'
     },
     {
       url: '/summary',
       name: 'Summary',
-      template: markdownUrlBase + 'templates/step8.md',
+      template: markdownUrlBase + 'templates/step14.md',
       hideCodeDiff: true
     }
   ]
