ac778d53aa05b9c11a7974bd61222bf59794b379
DAB0mB <emanor6@gmail.com>
2017-02-14 02:35:07 -0200

    added whatsapp clone - Ionic 2 and Meteor CLI version

---
+++ src/app/tutorials/angular2-whatsapp-meteor-cli/2.0.0.ts
@@ -0,0 +1,81 @@
+const markdownUrlBase = '/manuals/';
+
+export const V2_0_0 = {
+  isLatest: true,
+  urlName: '2.0.0',
+  displayName: 'Socially Merge',
+  dateString: '14.02.2017',
+  steps: [
+    {
+      url: '/setup',
+      name: 'Bootstrapping',
+      template: markdownUrlBase + 'templates/step1.md'
+    },
+    {
+      url: '/chats-page',
+      name: 'Chats page',
+      template: markdownUrlBase + 'templates/step2.md'
+    },
+    {
+      url: '/rxjs',
+      name: 'RxJS',
+      template: markdownUrlBase + 'templates/step3.md'
+    },
+    {
+      url: '/meteor-server-side',
+      name: 'Realtime Meteor Server',
+      template: markdownUrlBase + 'templates/step4.md'
+    },
+    {
+      url: '/folder-structure',
+      name: 'Folder Structure',
+      template: markdownUrlBase + 'templates/step5.md'
+    },
+    {
+      url: '/messages-page',
+      name: 'Messages Page',
+      template: markdownUrlBase + 'templates/step6.md'
+    },
+    {
+      url: '/authentication',
+      name: 'Authentication',
+      template: markdownUrlBase + 'templates/step7.md'
+    },
+    {
+      url: '/chats-mutations',
+      name: 'Chats Creation & Removal',
+      template: markdownUrlBase + 'templates/step8.md'
+    },
+    {
+      url: '/privacy',
+      name: 'Privacy & Security',
+      template: markdownUrlBase + 'templates/step9.md'
+    },
+    {
+      url: '/filter-and-pagination',
+      name: 'Filter & Pagination',
+      template: markdownUrlBase + 'templates/step10.md'
+    },
+    {
+      url: '/google-maps',
+      name: 'Google Maps',
+      template: markdownUrlBase + 'templates/step11.md'
+    },
+    {
+      url: '/file-upload',
+      name: 'File Upload & Images',
+      template: markdownUrlBase + 'templates/step12.md'
+    },
+    {
+      url: '/native-mobile',
+      name: 'Native Mobile',
+      template: markdownUrlBase + 'templates/step13.md'
+    },
+    {
+      url: '/summary',
+      name: 'Summary',
+      template: markdownUrlBase + 'templates/step14.md',
+      hideCodeDiff: true
+    }
+  ]
+};
---
+++ src/app/tutorials/angular2-whatsapp-meteor-cli/index.ts
@@ -0,0 +1,19 @@
+import {TutorialDefinition, ParsedPatchDefinition} from 'angular-meteor-tutorials-infrastructure';
+import {improveThisCodeResolver} from "../improve-code-resolver";
+import {Http} from "@angular/http";
+import { V2_0_0 } from './2.0.0';
+const markdownUrlBase = '/manuals/';
+
+export const ANGULAR2_WHATSAPP_METEOR_CLI: TutorialDefinition = {
+  id: 'whatsapp2-meteor-tutorial',
+  name: 'WhatsApp Clone with Ionic 2 and Meteor CLI',
+  gitHub: 'Urigo/Ionic2-MeteorCLI-WhatsApp',
+  baseRoute: 'meteor',
+  improveCodeUrlResolve: (tutorial: TutorialDefinition, patchDetails: ParsedPatchDefinition, filename: string, stepNumber: string, revision: string, http: Http) => {
+    return improveThisCodeResolver(markdownUrlBase, tutorial, patchDetails, filename, stepNumber, revision, http);
+  },
+  versions: {
+    // Git revision => version definition object
+    'master': V2_0_0
+  }
+};
